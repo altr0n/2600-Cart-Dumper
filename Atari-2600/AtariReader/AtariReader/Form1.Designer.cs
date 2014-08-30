491e02525352281953e2da7d23b54a8b0804818b
bit-smith <mta.alt03@gmail.com>
2014-08-30 15:45:59 -0400

    Adding Atari 2600 Project

---
+++ Atari-2600/AtariReader/AtariReader/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Find Ports";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Not Connected";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Close Port";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(8, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(196, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Get Rom!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 106);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Convert and Print";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(8, 135);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(196, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Save as .BIN";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 226);
+            this.progressBar1.Maximum = 8192;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(524, 23);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "\"Bin files|*.bin\"";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Location = new System.Drawing.Point(237, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 50);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ROM Size";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(6, 19);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(174, 21);
+            this.comboBox2.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(219, 194);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Comms Control";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Location = new System.Drawing.Point(237, 133);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(299, 73);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Mem Fetch Testing";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 21);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Get Location";
+            this.button7.UseVisualStyleBackColor = true;
+            
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 261);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "VRknight\'s Cart Readout Tool Suite";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Atari-2600/AtariReader/AtariReader/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox2.Items.Add("2k");
+            comboBox2.Items.Add("4k");
+            comboBox2.Items.Add("8k");
+        }
+         int RomSize = 0;
+        string[] ROM;
+          int[] ByteRom; 
+        byte[] test;
+        int index = 0;
+        SerialPort comm = new SerialPort();
+        int baud = 9600;
+        string PortName= "";
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(string element in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(element);
+            }
+            
+        }
+        public bool recieved = true;
+        bool reading = false;
+        public void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //if(reading)
+            //{
+            //    if (index >= RomSize)
+            //    {
+            //        MessageBox.Show("Done!");
+            //        comm.Close();
+            //        reading = false;
+                    
+            //        //printRom();
+            //    }
+                try
+                {
+                        
+                        string element = comm.ReadLine();
+                        //string[] st = element.Split(new char[] {':'});
+                        //int a = int.Parse(st[0], System.Globalization.NumberStyles.HexNumber);
+                        //int b = int.Parse(st[1], System.Globalization.NumberStyles.HexNumber);
+                    // MessageBox.Show(element.ToString());
+                        //MessageBox.Show(element.ToString("X2"));
+                        ROM[index] = element.ToString();
+                        index++;
+                        Invoke(new Action(() =>
+                        {
+                            label3.Text = "Value:" + element;
+                            progressBar1.Increment(1);
+                        }));
+ 
+                    
+                    
+                }
+                catch (IOException testing)
+                {
+                    MessageBox.Show("TEST");
+                }
+                catch(InvalidOperationException rc)
+                {
+
+                    MessageBox.Show("YUT");
+               }
+                
+            }
+               
+        
+
+        public void printRom()
+        {
+
+ 
+        }
+
+        public void Connect()
+        {
+            progressBar1.Value = 0;
+            PortName = comboBox1.SelectedItem.ToString();
+            comm = new SerialPort(PortName, baud);
+            try
+            {
+                label2.Text = "Attempting to open port";
+                comm.Open();
+            }
+            catch (IOException exc)
+            {
+                label2.Text = "Unable to open port";
+                MessageBox.Show("Unable to open port: " + exc.ToString());
+            }
+            if (comm.IsOpen)
+            {
+                label2.Text = "Connected!";
+            }
+            comm.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
+
+        }
+
+        public void Close()
+        {
+            comm.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comm.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string fetch = comboBox2.SelectedItem.ToString();
+            switch (fetch)
+            {
+                case "4k":
+                    RomSize = 4096;
+                    break;
+                case "2k":
+                    RomSize = 2048;
+                    break;
+                case "8k":
+                    RomSize = 8192;
+                    break;
+            }
+            ROM = new string[RomSize];
+            ByteRom = new int[RomSize];
+            test = new byte[RomSize] ;
+            comm.WriteLine("FF"); // Break the arduino out of it's loop!
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            convertRom();
+        }
+      
+        
+        public void convertRom()
+        {
+            DataTable testData = new DataTable();
+            progressBar1.Value = 0;
+            for (int t = 0; t < ROM.Length-1; t++)
+            {
+                ByteRom[t] = byte.Parse(ROM[t]);
+
+            }
+            for (int t = 0; t < ROM.Length-1; t++)
+            {
+
+              
+                    test[t] = (byte)ByteRom[t];
+                  
+                    progressBar1.Increment(1);
+                  
+            }
+            //for (int t = 0; t < test.GetLength(0); t++)
+            //{
+            //    string column = string.Format("Column{0}", t + 1);
+            //    dataGridView1.Columns.Add(column,column);
+            //}
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ByteArrayToFile(saveFileDialog1.FileName, test);
+            }
+            MessageBox.Show("Save Complete!");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {         
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ByteArrayToFile(saveFileDialog1.FileName, test);
+            }
+            MessageBox.Show("Save Complete!");
+        }
+
+        public bool ByteArrayToFile(string _FileName, byte[] _ByteArray)
+        {
+            label2.Text = "Writing to "+ _FileName+ " please wait...";
+            try
+            {
+                // Open file for reading
+                System.IO.FileStream _FileStream =
+                   new System.IO.FileStream(_FileName, System.IO.FileMode.Create,
+                                            System.IO.FileAccess.Write);
+                // Writes a block of bytes to this stream using data from
+                // a byte array.
+                _FileStream.Write(_ByteArray, 0, _ByteArray.Length);
+
+                // close file stream
+                _FileStream.Close();
+                label2.Text = "Wrote to " + _FileName + " enjoy!";
+                return true;
+            }
+            catch (Exception _Exception)
+            {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}",
+                                  _Exception.ToString());
+            }
+
+            // error occured, return false
+            return false;
+        }
+
+       
+      
+    }
+}
